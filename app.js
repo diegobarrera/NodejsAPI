22a73353e9119cbc471baba8126ce0f513c65fb3
Diego Barrera <dieg_goo@hotmail.com>
2018-09-19 17:25:34 -0500

    install eslint

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+"use strict";
+var express = require('express')
+var cookieParser = require('cookie-parser')
+var morgan = require('morgan')
+const winston = require('./configuration/winston')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined', {
+    stream: winston.stream
+}))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(cookieParser())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-module.exports = app;
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // add this line to include winston logging
+    winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
+
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
+})
+
+
+module.exports = app
