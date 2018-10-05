28f4a7ffa744383ad428d885b174ba79ecceecde
Diego Barrera <dieg_goo@hotmail.com>
2018-10-05 15:22:08 -0500

    Logging tool (#1)
    * install eslint
    
    * add gitignore
    
    * add gitignore
    
    * update gitignore
    
    * config winston
    
    * update ES6
    
    * add ci-cd
    
    * define env variable
    
    * remove nodemon dependecy

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+'use strict'
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+const winston = require('./configuration/winston')
+const app = express()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./src/routes/index')
+const usersRouter = require('./src/routes/index')
 
-var app = express();
+app.use(morgan('combined', {
+  stream: winston.stream
+}))
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: false
+}))
+app.use(cookieParser())
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const errorHandler = (err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-module.exports = app;
+  // add this line to include winston logging
+  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+}
+app.use(errorHandler)
+
+module.exports = app
