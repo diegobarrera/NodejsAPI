d1fd07dbae64af5b362a02c266e032c7f5c20cb1
Diego Barrera <dieg_goo@hotmail.com>
2018-10-09 16:24:50 -0500

    Develop (#3)
    * install eslint
    
    * add gitignore
    
    * add gitignore
    
    * update gitignore
    
    * config winston
    
    * update ES6
    
    * add ci-cd
    
    * define env variable
    
    * remove nodemon dependecy
    
    * add readme file
    
    * update readme
    
    * add dockerfile
    
    * fixes readme file definition
    
    * update gitignore
    
    * update gitignore
    
    * removed git cache
    
    * remove package lock
    
    * fix typo

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const errorHandler = (err, req, res, next) => {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // add this line to include winston logging
+  // winston error logging
   winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
 
   // render the error page
