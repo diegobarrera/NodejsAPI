0c30ddf2a4d5185737e9eb814bc3cf76a8b2e9c2
Diego Barrera <dieg_goo@hotmail.com>
2018-11-14 13:02:48 -0500

    add swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,28 @@
+const path = require('path')
 const express = require('express')
 const router = express.Router()
+const swaggerSpec = require('../docs/swaggerDef')
 
-/* GET home page. */
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: Healtcheck
+ *     responses:
+ *       200:
+ *         description: ok
+ */
 router.get('/', function (req, res, next) {
-  res.send('pong')
+  res.send('ok')
+})
+
+// Documentation
+router.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(swaggerSpec)
+})
+router.get('/docs', (req, res) => {
+    res.sendFile(path.join(__dirname, '../docs/index.html'))
 })
 
 module.exports = router
