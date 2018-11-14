f44a2bc9a40352dedb46bb06eaad57161587053f
Diego Barrera <dieg_goo@hotmail.com>
2018-11-14 14:24:06 -0500

    enviroment management

---
+++ src/configuration/env/development.js
@@ -0,0 +1,8 @@
+const configuration = {
+  database: {
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD
+  }
+}
+
+module.exports = configuration
---
+++ src/configuration/env/production.js
@@ -0,0 +1,7 @@
+const configuration = {
+  database: {
+
+  }
+}
+
+module.exports = configuration
